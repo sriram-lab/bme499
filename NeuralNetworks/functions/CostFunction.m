4dc68c4f8c511d06aa9acad005d16f59853aa579
Scott Campit <scampit@umich.edu>
2019-11-22 12:59:51 -0500

    add all

--- NeuralNetworks/functions/CostFunction.m
+++
@@ -1,7 +0,0 @@
-function [cost] = CostFunction(AL, Y)
-    % CostFunction implements a cost function for n layer neural networks
-    m = shape(Y, 2);
-    A = 
-    cost = -1*sum(dot(Y, log(AL') + dot(1-Y, log(1-A))))/m;
-
-end
